75db16448eea3b821d50e1279eb6d40adca7a660
Fabriece Sumuni <sfabriece@hotmail.com>
2015-01-16 03:12:37 +0100

    Update Weather.java
    removed api key

--- main/Weather.java
+++ main/Weather.java
@@ -11,7 +11,7 @@ public class Weather {
 	
 public void updateWeather(){
 
-ForecastIO fio = new ForecastIO("da289976f0020ae24853417ab9a6c011"); //instantiate the class with the API key. 
+ForecastIO fio = new ForecastIO(" can't be public"); //instantiate the class with the API key. 
 fio.setUnits(ForecastIO.UNITS_SI);             //sets the units as SI - optional
 fio.setExcludeURL("hourly,minutely");             //excluded the minutely and hourly reports from the reply
 fio.getForecast("58.937861", "5.702062");   //sets the latitude and longitude - not optional
